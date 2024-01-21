82220433b99485f64b2e7f616284cbd42a3f5255
Andiiiiie <andiejessica2020@gmail.com>
2024-01-21 18:55:55 +0300

    crud marque

--- src/app/(core)/(administration)/marques/page.tsx
+++ src/app/(core)/(administration)/marques/page.tsx
@@ -1,25 +1,40 @@
 'use client';
-import {useGet} from "@/app/(core)/utils/hooks";
+import {sendDelete, useGet} from "@/app/(core)/utils/hooks";
 import {API_URL} from "@/app/config";
 import React from "react";
 import {Column} from "primereact/column";
 import Table from "@/app/(core)/ui/Table";
 import FaIcon from "@/app/(core)/ui/FaIcon";
-import {faEdit, faPlus, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
+import {faEdit, faPlus, faTrashAlt, faWarning} from "@fortawesome/free-solid-svg-icons";
+import ConfirmationModal from "@/app/(core)/ui/ConfirmationModal";
 export default function Marques()
 {
     const [data] = useGet(API_URL+"marques");
 
-    function Actions() {
+    function Delete(id: string) {
+        sendDelete(`${API_URL}marques/${id}`);
+        window?.location?.reload();
+    }
+
+    function Actions(row: any) {
         return (
-            <p className="p-buttonset">
-                <a href={""} className={"btn btn-warning btn-icon mx-1"}>
-                    <FaIcon icon={faEdit} />
-                </a>
-                <a href={""} className={"btn btn-danger btn-icon mx-1"}>
-                    <FaIcon icon={faTrashAlt} />
-                </a>
-            </p>
+            <>
+                <p className="p-buttonset">
+                    <a href={`/marques/modifier/${row.id}`} className={"btn btn-warning btn-icon mx-1"}>
+                        <FaIcon icon={faEdit}/>
+                    </a>
+                    <button data-bs-target={`#/marques/supprimer/${row.id}`} className={"btn btn-danger btn-icon mx-1"}
+                            data-bs-toggle="modal">
+                        <FaIcon icon={faTrashAlt}/>
+                    </button>
+                </p>
+                <ConfirmationModal id={`/marques/supprimer/${row.id}`}
+                                   title={"Confirmer la suppression"}
+                                   message={"Supprimer la marque supprimera toutes les données qui lui sont liées."}
+                                   type="danger" icon={faWarning}
+                                   action={()=>{Delete(row.id)}}
+                                   actionButton={"Supprimer"} />
+            </>
         );
     }
 
@@ -47,7 +62,6 @@ export default function Marques()
                         </div>
                         <div className="card-body overflow-hidden">
                             <Table data={data}>
-                                <Column field="id" header="Id" sortable filter/>
                                 <Column field="nom" header="Nom" sortable filter/>
                                 <Column header="Actions" body={Actions} style={{width: "10%"}}/>
                             </Table>
